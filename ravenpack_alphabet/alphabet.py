0ecfc8ada9175b25aacb3101061c5931a5fded19
Javier Del saz <javierdelsaz@Javiers-MacBook-Pro.local>
2020-07-30 01:51:53 +0200

    Code Styling

--- ravenpack_alphabet/alphabet.py
+++ ravenpack_alphabet/alphabet.py
@@ -9,6 +9,14 @@ def check_word(word_to_make, available_alphabet):
     return letter_not_available is False
 
 
+def __make_letters_repository(available_alphabet):
+    letters_repository = {}
+    for letter in available_alphabet:
+        letters_repository[letter] = (letters_repository[letter] + 1) if letter in letters_repository else 1
+
+    return letters_repository
+
+
 def __is_letter_unavailable(letter, letters_repository):
     if letter in letters_repository:
         if letters_repository[letter] == 0:
@@ -18,11 +26,3 @@ def __is_letter_unavailable(letter, letters_repository):
             return False
     else:
         return True
-
-
-def __make_letters_repository(available_alphabet):
-    letters_repository = {}
-    for letter in available_alphabet:
-        letters_repository[letter] = (letters_repository[letter] + 1) if letter in letters_repository else 1
-
-    return letters_repository
