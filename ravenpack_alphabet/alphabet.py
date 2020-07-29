96a5bbf27044d70e75a527c9a6a8b9196f819e4f
Javier Del saz <javierdelsaz@Javiers-MacBook-Pro.local>
2020-07-29 13:01:01 +0200

    Algorithm implementation

---
+++ ravenpack_alphabet/alphabet.py
@@ -0,0 +1,28 @@
+def check_word(word_to_make, available_alphabet):
+    if not word_to_make:
+        return False
+
+    letters_repository = __make_letters_repository(available_alphabet)
+
+    letter_not_available = any(__is_letter_unavailable(letter, letters_repository) for letter in word_to_make)
+
+    return letter_not_available is False
+
+
+def __is_letter_unavailable(letter, letters_repository):
+    if letter in letters_repository:
+        if letters_repository[letter] == 0:
+            return True
+        else:
+            letters_repository[letter] -= 1
+            return False
+    else:
+        return True
+
+
+def __make_letters_repository(available_alphabet):
+    letters_repository = {}
+    for letter in available_alphabet:
+        letters_repository[letter] = (letters_repository[letter] + 1) if letter in letters_repository else 1
+
+    return letters_repository
